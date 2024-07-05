8f6510940d374b759c8dcd04e81355c5ee4c3b0e
Ozan <gulbasozan@example.com>
2024-07-05 15:31:17 +0300

    Setup Distance Sensor Reading and implement basic safety measure

--- rto-avatar/include/rto-avatar/SafeOmniDrive.h
+++ rto-avatar/include/rto-avatar/SafeOmniDrive.h
@@ -5,6 +5,10 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
+#include "sensor_msgs/msg/point_cloud.hpp"
+#include <mutex>
+
+using namespace std;
 
 class SafeOmniDrive: public rec::robotino::api2::OmniDrive
 {
@@ -14,8 +18,11 @@ class SafeOmniDrive: public rec::robotino::api2::OmniDrive
 
     private:
         rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr safe_cmd_vel_sub_;
+        rclcpp::Subscription<sensor_msgs::msg::PointCloud>::SharedPtr distance_sensors_sub_;
 
         std::string parent_node_name_;
+        sensor_msgs::msg::PointCloud distance_sensor_readings_;
+        std::mutex distance_sensor_mutex_;
 
         double max_linear_vel_;
         double min_linear_vel_;
@@ -23,6 +30,9 @@ class SafeOmniDrive: public rec::robotino::api2::OmniDrive
         double min_angular_vel_;
 
         void safeCmdVelCallback(const geometry_msgs::msg::Twist::SharedPtr msg);
+        void distanceSensorsCallback(const sensor_msgs::msg::PointCloud::SharedPtr msg);
+        bool isInsideSafetyBubble(double point_x, double point_y, double safety_radius);
+        string boolToString(bool x);
 
     public:
         void setMaxMin( double max_linear_vel, double min_linear_vel,
--- rto-avatar/src/SafeOmniDrive.cpp
+++ rto-avatar/src/SafeOmniDrive.cpp
@@ -1,4 +1,6 @@
 #include "SafeOmniDrive.h"
+#include <cmath>
+#include <sstream>
 
 using std::placeholders::_1;
 
@@ -6,17 +8,36 @@ SafeOmniDrive::SafeOmniDrive(rclcpp::Node* parent_node)
 {
 	parent_node_name_ = std::string(parent_node->get_name());
     safe_cmd_vel_sub_ = parent_node->create_subscription<geometry_msgs::msg::Twist>("rto3/safe_cmd_vel", 10, std::bind(&SafeOmniDrive::safeCmdVelCallback, this, _1));
+	distance_sensors_sub_ = parent_node->create_subscription<sensor_msgs::msg::PointCloud>("rto3/distance_sensors", 10, std::bind(&SafeOmniDrive::distanceSensorsCallback, this, _1));
 }
 
 SafeOmniDrive::~SafeOmniDrive()
 {}
 
+void SafeOmniDrive::distanceSensorsCallback(const sensor_msgs::msg::PointCloud::SharedPtr msg)
+{
+	std::lock_guard<std::mutex> lock(distance_sensor_mutex_);
+	distance_sensor_readings_ = *msg;
+}
+
+bool SafeOmniDrive::isInsideSafetyBubble(double point_x, double point_y, double safety_radius)
+{
+	return pow(safety_radius, 2) >= pow(point_x, 2) + pow(point_y, 2);
+
+}
+
+string SafeOmniDrive::boolToString(bool x)
+{
+	return x ? "true" : "false";
+}
 
 void SafeOmniDrive::safeCmdVelCallback(const geometry_msgs::msg::Twist::SharedPtr msg)
 {
     double linear_x = msg->linear.x;
 	double linear_y = msg->linear.y;
 	double angular = msg->angular.z;
+
+	double safety_radius = 0.4;
 
 	if ( fabs( linear_x ) > max_linear_vel_ )
 	{
@@ -63,8 +84,27 @@ void SafeOmniDrive::safeCmdVelCallback(const geometry_msgs::msg::Twist::SharedPt
 			angular = -min_angular_vel_;
 	}
 
-	RCLCPP_INFO(rclcpp::get_logger(parent_node_name_), "Callback Function Triggered");
-	setVelocity( linear_x, linear_y, angular);
+	std::lock_guard<std::mutex> lock(distance_sensor_mutex_);
+	if (!distance_sensor_readings_.points.empty())
+	{
+		bool safety_breach = false;
+
+		for (unsigned int i = 0; i < 9; ++i)
+		{
+			safety_breach = safety_breach | isInsideSafetyBubble(
+												distance_sensor_readings_.points[i].x,
+												distance_sensor_readings_.points[i].y,
+												safety_radius);
+		}
+		if (!safety_breach)
+			setVelocity( linear_x, linear_y, angular);
+		
+		std::ostringstream os;
+		os << "SafetyBreach: " << boolToString(safety_breach); 
+		RCLCPP_INFO(rclcpp::get_logger(parent_node_name_), "%s", os.str().c_str());
+	}
+
+	// RCLCPP_INFO(rclcpp::get_logger(parent_node_name_), "Callback Function Triggered");
 }
 
 void SafeOmniDrive::setMaxMin( double max_linear_vel, double min_linear_vel,
